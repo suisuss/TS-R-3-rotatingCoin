255539957938b9b02f9f084f3ad6d35a74dc3b7c
Jacob Sussmilch <suisuss@protonmail.com>
2021-08-23 14:52:09 +1000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <p>suisuss/TS-R-3-rotatingCoin</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,83 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 2
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setClearColor(0xffffff, 0);
+renderer.setClearAlpha(.1)
+renderer.domElement.style.position = "fixed"
+renderer.domElement.style.top = "0"
+renderer.domElement.style.left = "0"
+document.body.appendChild(renderer.domElement)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+camera.position.setZ(30);
+camera.position.setX(-3);
+
+
+// Torus
+
+const geometry = new THREE.CylinderBufferGeometry(15, 15, 2, 100);
+const material = new THREE.MeshBasicMaterial({ color: 0x999999, wireframe: true, transparent: true, opacity: 0.85 })
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+
+
+
+// Helpers
+
+//const lightHelper = new THREE.PointLightHelper(pointLight)
+//const gridHelper = new THREE.GridHelper(200, 50);
+//scene.add(lightHelper, gridHelper)
+
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  render()
+}
+
+function render() {
+  renderer.render(scene, camera)
+}
+
+function animate() {
+  requestAnimationFrame(animate)
+
+  torus.rotation.x += 0.01
+  torus.rotation.y += 0.01
+
+  controls.update()
+
+  render()
+}
+animate()
